f0feda9f9a1b66b81eb5d3dfd4dcba9c9a3b4272
Tihomir Surdilovic <tihomir@temporal.io>
2023-09-09 09:00:29 -0400

    boilerplate
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

---
+++ src/main/java/io/temporal/replaydemo/ReplaydemoApplication.java
@@ -0,0 +1,11 @@
+package io.temporal.replaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplaydemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReplaydemoApplication.class, args).start();;
+	}
+}
---
+++ src/test/java/io/temporal/replaydemo/ReplaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.temporal.replaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
